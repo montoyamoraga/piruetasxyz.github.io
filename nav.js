2f5ab3747c9221f5f2abc23c69011bb82f5a54d1
montoyamoraga <montoyamoraga@gmail.com>
2025-07-29 15:32:52 -0400

    agregar projects/

--- nav.js
+++ nav.js
@@ -1,14 +1,29 @@
 let navbar = `
  <nav class="navegacion">
-    <h3>
+    <h1>
     <a href="./" id="english">en</a> /
     <a href="./" id="espanol">es</a>
-    </h3>
-    <h3><a href="/">home</a></h3>
-    <h3><a href="/store" data-i18n="store"></a></h3>
-    <h3><a href="/projects" data-i18n="projects"></a></h3>
-    <h3><a href="/people" data-i18n="people"></a></h3>
-    <h3><a href="/info">info</a></h3>
+    </h1>
+    <h1><a href="/">home</a></h1>
+    <h1><a href="/store" data-i18n="store"></a></h1>
+    <h1><a href="/info">info</a></h1>
+    <h1><a href="/people" data-i18n="people"></a></h1>
+    <h1 data-i18n="projects"></h1>
+    <h3>software</h3>
+    <ol>
+        <li>
+            <a href="/projects/maquinitas-tidal" data-i18n="projects-maquinitas-tidal">
+            </a>
+        </li>
+    </ol>
+    <h3>hardware</h3>
+    <ol>
+        <li>
+            <a href="/projects/gerassic-organ" data-i18n="projects-gerassic-organ">
+            </a>
+        </li>
+    </ol>
+    <h3>teaching</h3>
     </nav>
 `;
 
